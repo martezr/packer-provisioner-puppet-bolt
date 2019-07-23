7e601bf8a9018f7a11c2f6592b5796dfdb760abe
martezr <martez.reed@greenreedtech.com>
2019-07-23 12:22:23 -0500

    Format Golang code

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/packer/packer/plugin"
-  "github.com/martezr/packer-provisioner-puppet-bolt/bolt"
+	"github.com/martezr/packer-provisioner-puppet-bolt/bolt"
 )
 
 func main() {
